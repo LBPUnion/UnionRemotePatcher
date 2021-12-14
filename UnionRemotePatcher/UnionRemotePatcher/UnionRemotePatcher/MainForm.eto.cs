97783002282b968f1889c31fdf6d6f4c0e30bc43
Logan Lowe <loganr.lowe@gmail.com>
2021-12-14 15:06:25 -0700

    Add code
    we have a simple UI that I completely ripped straight outta UnionPatcher and changed up a bit but I have a pretty decent start

---
+++ UnionRemotePatcher/UnionRemotePatcher/UnionRemotePatcher/MainForm.eto.cs
@@ -0,0 +1,148 @@
+using Eto;
+using Eto.Drawing;
+using Eto.Forms;
+using System;
+using System.Diagnostics;
+
+namespace UnionRemotePatcher
+{
+    partial class MainForm : Form
+    {
+        private TextBox ps3LocalIP;
+        private TextBox lbpGameID;
+        private TextBox serverUrl;
+        public Dialog CreateOkDialog(string title, string errorMessage)
+        {
+            DynamicLayout layout = new();
+            Button button;
+
+            layout.Spacing = new Size(5, 5);
+            layout.MinimumSize = new Size(350, 100);
+
+            layout.BeginHorizontal();
+            layout.Add(new Label
+            {
+                Text = errorMessage,
+            });
+
+            layout.BeginHorizontal();
+            layout.BeginVertical();
+            layout.Add(null);
+            layout.Add(button = new Button
+            {
+                Text = "OK",
+            });
+
+            layout.EndVertical();
+            layout.EndHorizontal();
+            layout.EndHorizontal();
+
+            Dialog dialog = new()
+            {
+                Content = layout,
+                Padding = new Padding(10, 10, 10, 10),
+                Title = title,
+            };
+
+            button.Click += delegate {
+                dialog.Close();
+            };
+
+            return dialog;
+        }
+
+        public Control CreatePatchButton(int tabIndex = 0)
+        {
+            Button control = new()
+            {
+                Text = "Patch!",
+                TabIndex = tabIndex,
+            };
+
+            control.Click += delegate {
+                if (string.IsNullOrEmpty(this.ps3LocalIP.Text))
+                {
+                    this.CreateOkDialog("Form Error", "No PS3 IP address specified!").ShowModal();
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(this.lbpGameID.Text))
+                {
+                    this.CreateOkDialog("Form Error", "No game ID specified!").ShowModal();
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(this.serverUrl.Text))
+                {
+                    this.CreateOkDialog("Form Error", "No server URL specified!").ShowModal();
+                    return;
+                }
+
+                try
+                {
+                    //Patcher.PatchFile(this.filePicker.FilePath, this.serverUrl.Text, this.outputFileName.FilePath);
+                    RemotePatch.EBOOTRemotePatch(this.ps3LocalIP.Text, this.lbpGameID.Text, this.serverUrl.Text);
+                }
+                catch (Exception e)
+                {
+                    this.CreateOkDialog("Error occurred while patching", "An error occured while patching:\n" + e).ShowModal();
+                    return;
+                }
+
+                this.CreateOkDialog("Success!", $"The Server URL for {this.lbpGameID.Text} on the PS3 at {this.ps3LocalIP.Text} has been patched to {this.serverUrl.Text}").ShowModal();
+            };
+
+            return control;
+        }
+
+        public Control CreateHelpButton(int tabIndex = 0)
+        {
+            Button control = new()
+            {
+                Text = "Help",
+                TabIndex = tabIndex,
+            };
+
+            control.Click += delegate {
+                Process process = new();
+
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.FileName = "https://www.lbpunion.com";
+                process.Start();
+            };
+
+            return control;
+        }
+        void InitializeComponent()
+        {
+            Title = "UnionRemotePatcher - LittleBigPlanet Remote EBOOT Patcher";
+            MinimumSize = new Size(400, 200);
+            Padding = 10;
+
+            Content = new TableLayout
+            {
+                Spacing = new Size(5, 5),
+                Padding = new Padding(10, 10, 10, 10),
+                Rows = {
+                    new TableRow(
+                        new TableCell(new Label { Text = "PS3 Local IP: ", VerticalAlignment = VerticalAlignment.Center }),
+                        new TableCell(this.ps3LocalIP = new TextBox { TabIndex = 0 })
+                    ),
+                    new TableRow(
+                        new TableCell(new Label { Text = "Game ID: ", VerticalAlignment = VerticalAlignment.Center }),
+                        new TableCell(this.lbpGameID = new TextBox { TabIndex = 1 })
+                    ),
+                    new TableRow(
+                        new TableCell(new Label { Text = "Server URL: ", VerticalAlignment = VerticalAlignment.Center }),
+                        new TableCell(this.serverUrl = new TextBox { TabIndex = 2 })
+                    ),
+                    new TableRow(
+                        new TableCell(this.CreateHelpButton(3)),
+                        new TableCell(this.CreatePatchButton(4))
+                    ),
+                },
+
+            };
+        }
+    }
+}
---
+++ UnionRemotePatcher/UnionRemotePatcher/UnionRemotePatcher/RemotePatch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LBPUnion.UnionPatcher;
+
+namespace UnionRemotePatcher
+{
+    public static class RemotePatch
+    {
+        public static void EBOOTRemotePatch(string ps3ip, string gameID, string serverURL)
+        {
+        
+        }
+    }
+}
