d8e47d5a8cff6f12c1d2589a475d4be6f44d470c
Logan Lowe <loganr.lowe@gmail.com>
2021-12-14 15:24:26 -0700

    i forgot the gitignore
    blyat

--- UnionRemotePatcher/UnionRemotePatcher/UnionRemotePatcher/RemotePatch.cs
+++ UnionRemotePatcher/UnionRemotePatcher/UnionRemotePatcher/RemotePatch.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LBPUnion.UnionPatcher;
+using System.IO;
+using System.Net;
 
 namespace UnionRemotePatcher
 {
-    public static class RemotePatch
+    public class RemotePatch
     {
+        private static void DownloadFile(string url, string path)
+        {
+            WebClient cln = new WebClient();
+            cln.DownloadFile(url, path);
+        }
+
         public static void EBOOTRemotePatch(string ps3ip, string gameID, string serverURL)
         {
-        
+            Directory.CreateDirectory(@"Files");
+            DownloadFile("https://www.psx-place.com/resources/trueancestor-self-resigner-by-jjkkyu.33/download?version=44", @"Files/TrueAncestor.tmp");
+
         }
     }
 }
